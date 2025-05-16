3c3b68dad7c0626adbb1447ec6536bec419e8778
Filipo Alefosio <filipo_alefosio@hotmail.com>
2025-05-16 21:04:40 +1200

    adjusting app.js with chakra

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Box, Flex, Heading, Button } from '@chakra-ui/react';
 import Home from './homePage';
-import About from './aboutPage'; // Make sure this file exists
+import About from './aboutPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
+      <Box p={4}>
+        <Flex as="nav" justify="space-between" align="center" mb={6}>
+          <Heading size="md">My Website</Heading>
+          <Flex gap={4}>
+            <Button as={Link} to="/" colorScheme="teal" variant="ghost">
+              Home
+            </Button>
+            <Button as={Link} to="/about" colorScheme="teal" variant="ghost">
+              About
+            </Button>
+          </Flex>
+        </Flex>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
   );
 }
 
-export default App; // Export App as default
+export default App;
