ea2dbd48d1ba2c43195408146f146446e81384bf
Filipo Alefosio <filipo_alefosio@hotmail.com>
2025-06-04 21:07:35 +1200

    adjusting my app.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Box, Flex, Heading, Button } from '@chakra-ui/react';
 import Home from './homePage';
 import About from './aboutPage';
+import ColorModeSwitcher from './ColorModeSwitcher';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/playground">
       <Box p={4}>
         <Flex as="nav" justify="space-between" align="center" mb={6}>
           <Heading size="md">My Website</Heading>
-          <Flex gap={4}>
+          <Flex gap={4} align="center">
             <Button as={Link} to="/" colorScheme="teal" variant="ghost">
               Home
             </Button>
             <Button as={Link} to="/about" colorScheme="teal" variant="ghost">
               About
             </Button>
+            <ColorModeSwitcher />
           </Flex>
         </Flex>
 
